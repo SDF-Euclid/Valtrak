f4f468364e0c1c1f1ddf392deb8546975d1d2070
Eli Johnson <186127338+SDF-Euclid@users.noreply.github.com>
2026-04-14 14:51:09 -0500

    Initial file structure

---
+++ Valtrak/src/main/java/com/example/valtrak/ValtrakApplication.java
@@ -0,0 +1,13 @@
+package com.example.valtrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValtrakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValtrakApplication.class, args);
+    }
+
+}
