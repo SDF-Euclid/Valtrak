03ae523708532244b7614f10ef78a6e2fe4e1174
Eli Johnson <186127338+SDF-Euclid@users.noreply.github.com>
2026-04-15 02:15:58 -0500

    Update ValtrakApplication.java

--- src/main/java/com/example/valtrak/ValtrakApplication.java
+++ src/main/java/com/example/valtrak/ValtrakApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ValtrakApplication {
-
+    @SuppressWarnings("UnnecessaryModifier")
     public static void main(String[] args) {
-        SpringApplication.run(ValtrakApplication.class, args);
-    }
-
+        SpringApplication.run(ValtrakApplication.class, args);}
 }
